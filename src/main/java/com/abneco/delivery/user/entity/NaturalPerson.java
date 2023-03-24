7a4a039c1dba03a504819284f0fc09299690f647
Abner Werley <abnerwerley77@gmail.com>
2023-03-24 15:49:05 -0300

    :construction: construction: basic relationship beween purhcases, buyers and products

--- src/main/java/com/abneco/delivery/user/entity/NaturalPerson.java
+++ src/main/java/com/abneco/delivery/user/entity/NaturalPerson.java
@@ -3,21 +3,11 @@ package com.abneco.delivery.user.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
 public class NaturalPerson extends User {
 
-    @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    private String id;
     private Long cpf;
 }
