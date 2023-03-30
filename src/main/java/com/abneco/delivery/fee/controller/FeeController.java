6ef8291ae163b4a0148b4b6bc1e4a0672541db5a
Abner Werley <abnerwerley77@gmail.com>
2023-03-29 22:59:39 -0300

    :recycle: refactor: FeeService only returns the fee now

--- src/main/java/com/abneco/delivery/fee/controller/FeeController.java
+++ src/main/java/com/abneco/delivery/fee/controller/FeeController.java
@@ -1,7 +1,5 @@
 package com.abneco.delivery.fee.controller;
 
-
-import com.abneco.delivery.fee.dto.FeeResponse;
 import com.abneco.delivery.fee.dto.CepForm;
 import com.abneco.delivery.fee.service.FeeService;
 import lombok.AllArgsConstructor;
@@ -9,6 +7,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @AllArgsConstructor
@@ -21,7 +21,7 @@ public class FeeController {
 
     @PostMapping("/consulta-endereco")
     @ResponseStatus(HttpStatus.OK)
-    public FeeResponse getDeliveryFeeByCep(@RequestBody(required = true) CepForm form) {
+    public BigDecimal getDeliveryFeeByCep(@RequestBody CepForm form) {
         return service.generateResponse(form.getCep());
     }
 }
